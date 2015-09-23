f172ae7a0ce2fcf76be455068efd6773b28d08d9
Jonas Hagstedt <hagstedt@gmail.com>
2015-09-23 15:12:42 +0200

    remove duplicate server start for CI

--- runtests-ci.sh
+++ runtests-ci.sh
@@ -5,9 +5,7 @@ npm install react
 cd ..
 cd ..
 
-
 # Start the template server
-node ./javascript/dist/template-server.js socketPath=./template-server.sock &
-
 nohup bash -c "node ./javascript/dist/template-server.js socketPath=./template-server.sock 2>&1 &" && sleep 2
+
 ./runtests.sh
